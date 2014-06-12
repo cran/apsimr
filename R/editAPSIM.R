3b00516a6bc4930ca7a6e48b48e6728266a8f5e4
Bryan Stanfill <bryan.stanfill@csiro.au>
2014-06-12 00:00:00 +0000

    version 0.1

---
+++ R/editAPSIM.R
@@ -0,0 +1,177 @@
+#' Edit an APSIM simulation
+#' 
+#' The variables specified by \code{var} within the .apsim file specified by \code{file} 
+#' in the working directory \code{wd} are edited. The old values are replaced with \code{value}, which
+#' is a list that has the same number of elements as the length of the vector \code{var}.  The current
+#' .apsim file will be overwritten if \code{overwrite} is set to \code{TRUE}; otherwise the file
+#' \emph{file-edited.apsim} will be created.  The name of the written file is returned.
+#' 
+#' @name edit_apsim
+#' @param file file ending in .apsim to be edited
+#' @param wd directory containing the .apsim file to be edited; defaults to the current wd
+#' @param var vector of variables to be edited
+#' @param value list of new values for the specified variables
+#' @param overwrite logical; if \code{TRUE} the old file is overwritten, a new file is written otherwise
+#' @return character string containing the resulting XML content
+#' @examples
+#' \dontrun{
+#' #The file I want to edit is called "Canopy.apsim" which is in the directory "~/APSIM"
+#' file <- "Canopy.apsim"
+#' wd <- "~/APSIM"
+#' 
+#' #I want to change the Thickness of the Soilwater, the SoilCN of the SoilOrganicMatter and
+#' #the state at which the simulation is being run.
+#' var <- c("SoilWater/Thickness", "SoilOrganicMatter/SoilCN", "State")
+#' 
+#' #Change SoilWater-Thickness to 200,200,300x9
+#' #Change SoilCN to 10
+#' #Change "State" to "NSW"
+#' value <- list(c(rep(200, 2), rep(300, 9)), 10, "NSW")
+#' 
+#' #Edit the apsim file without overwriting it
+#' edit_apsim(file, wd, var, value, overwrite = FALSE)
+#' 
+#' #Run the edited simulation
+#' exe <-"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe"
+#' 
+#' results <- apsim(exe, wd, files = "Canopy-edited.apsim")
+#' }
+
+edit_apsim <- function(file, wd = getwd(), var, value, overwrite = FALSE){
+  
+  oldWD<-getwd()
+  setwd(wd)
+  
+  fileNames <- dir(,pattern=".apsim$")
+  
+  if(length(fileNames)==0){
+    stop("There are no .apsim files in the folder wd to edit.")
+  }
+  
+  file<-match.arg(file,fileNames,several.ok=TRUE)
+  
+  pXML<-xmlParse(file)
+  
+  for(i in 1:length(var)){
+    
+    vari<-pXML[[paste("//",var[i],sep="")]]
+    
+    #If supplied length is shorter then length to replace, then
+    #replicate the last value enough times to fill the void, give message
+    lToReplace<-xmlSize(vari)
+    lReplace<-length(value[[i]])
+    lenDiff<-lToReplace-lReplace
+    
+    if(lenDiff>0){
+      value[[i]]<-c(value[[i]],rep(value[[i]][lReplace],lenDiff))
+      warning(paste("Supplied values for",var[i],"was too short",sep=" "))
+    }
+    
+    for(j in 1:lToReplace){
+      xmlValue(vari[[j]])<-as.character(value[[i]][j])
+    }
+    
+  }
+  
+  if(overwrite){
+    setwd(oldWD)
+    return(saveXML(pXML,file=file))
+  }else{
+    
+    #Remove .apsim tag if present and add edited tag
+    newName<-paste(gsub(".apsim","",file),"-edited",sep="")
+    
+    #Rename the simulation
+    wholeSim<-pXML[["//simulation"]]    
+    xmlAttrs(wholeSim)<-c(name=newName)
+    
+    #Rename the output filename to match the new file name
+    outName<-pXML[["//outputfile/filename"]]
+    xmlValue(outName)<-paste(newName,".out",sep="")
+    
+    #Also update title for output file
+    outTitle<-pXML[["//outputfile/title"]]
+    xmlValue(outTitle)<-newName
+    
+    setwd(oldWD)
+    return(saveXML(pXML,file=paste(newName,".apsim",sep="")))
+  }
+}
+
+#' Edit an APSIM module file
+#' 
+#' APSIM uses .xml files to dictate how certain processes are carried out.  Similar to
+#' \code{\link{edit_apsim}} this function edits a file that will be used in an APSIM simulation.  Unlike
+#' \code{\link{edit_apsim}} this function edits the .xml simulation files.
+#' The variables specified by \code{var} within the .xml file specified by \code{file} 
+#' in the working directory \code{wd} are edited. The old values are replaced with \code{value}, which
+#' is a list that has the same number of elements as the vector \code{var} is long.  The current
+#' .xml file will be overwritten if \code{overwrite} is set to \code{TRUE}; otherwise the file
+#' \emph{file-edited.xml} will be created.  The name of the written file is returned.
+#' 
+#' @name edit_sim_file
+#' @param file .xml module file to be edited
+#' @param wd directory containing the .xml file to be edited; defaults to the current wd
+#' @param var vector of variables to be edited
+#' @param value list of new values for the specified variables
+#' @param overwrite logical; if \code{TRUE} the old file is overwritten, a new file is written otherwise
+#' @return character string containing the resulting XML content
+#' @examples
+#' \dontrun{
+#' #The file I want to edit is called "Soil.xml" which is the the directory "~/APSIM"
+#' file <- "Soil.xml"
+#' wd <- "~/APSIM"
+#' 
+#' #I want to change the potential nitrification and N2O from nitrification
+#' var <- c("nitrification_pot", "dnit_nitrf_loss")
+#' 
+#' #Change both to absolute values of random N(0,1) 
+#' value <- list(abs(rnorm(1)), abs(rnorm(1)))
+#' 
+#' #Edit Soil.xml without overwriting it
+#' edit_sim_file(file, wd, var, value, overwrite = FALSE)
+#' }
+
+edit_sim_file <- function(file, wd = getwd(), var, value, overwrite = FALSE){
+  
+  oldWD<-getwd()
+  setwd(wd)
+  
+  if(!(file%in%list.files())){
+    stop("Specified file could not be found in the current working directory.")
+  }
+  
+  pXML<-xmlParse(file)
+  
+  for(i in 1:length(var)){
+    
+    vari<-pXML[paste("//",var[i],sep="")]
+    
+    #If supplied length is shorter then length to replace, then
+    #replicate the last value enough times to fill the void, give message
+    lToReplace<-xmlSize(vari)
+    lReplace<-length(value[[i]])
+    lenDiff<-lToReplace-lReplace
+    
+    if(lenDiff>0){
+      value[[i]]<-c(value[[i]],rep(value[[i]][lReplace],lenDiff))
+      #warning(paste("Supplied values for",var[i],"was too short",sep=" "))
+    }
+    
+    for(j in 1:lToReplace){
+      xmlValue(vari[[j]])<-as.character(value[[i]][j])
+    }
+    
+  }
+  
+  if(overwrite){
+    setwd(oldWD)
+    return(saveXML(pXML,file=file))
+  }else{
+    
+    #Remove .apsim tag if present and add edited tag
+    newName<-paste(gsub(".xml","",file),"-edited",sep="")
+    setwd(oldWD)
+    return(saveXML(pXML,file=paste(newName,".xml",sep="")))
+  }  
+}
---
+++ R/runAPSIM.R
@@ -0,0 +1,160 @@
+#' Run APSIM simulations from R
+#' 
+#' This function will run APSIM from R.  The only required input is the 
+#' file path to the APSIM executable.  It is assumed the current working directory contains the .apsim file(s)
+#' to be run.  If that is not the case then the directory containing the .apsim file(s) to be run
+#' can to be specified by \code{wd}.  One can specify a list of .apsim files to be run within the
+#' directory \code{wd} using the \code{files} argument, otherwise all .apsim files within \code{wd} are run. 
+#' The results for each .apsim file that is run is an element of the list that is returned.  
+#' Each element of the list is of the class \code{"apsim"}.
+#' 
+#' @name apsim
+#' @param exe  path to the APSIM executable
+#' @param wd  working directory containing the .apsim files to be run; defaults to the current working directory
+#' @param files  .apsim files to be run; if left empty all .apsim files in \code{wd} will be run
+#' @return list of output files; each element corresponds to an .apsim file
+#' @export
+#' @examples
+#' 
+#' \dontrun{
+#' exe <-"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe"
+#' wd <- "~/APSIM"
+#' to_run <- c("Centro.apsim", "Continuous Wheat.apsim")
+#' results <- apsim(exe, wd, files = to_run)
+#' }
+
+apsim<-function(exe, wd = getwd(), files = NULL){
+  
+  exe<-addCommas(exe) #If there are spaces in the path to APSIM.exe, they need to be added
+  oldWD<-getwd()
+  setwd(wd)
+
+  fileNames <- dir(,pattern=".apsim$")
+
+  if(length(fileNames)==0){
+    stop("There are no .apsim files in the folder wd to run.")
+  }
+  
+  if(is.null(files)){
+    
+    #If files is left NULL then run every .apsim file in the provided directory
+    files<- fileNames
+    
+  }else{
+    
+    nFiles<-length(files)
+    #Allow for abbreviations and check the files are in there
+    files<-match.arg(files,fileNames,several.ok=TRUE)
+    if(nFiles != length(files))
+      warning("Not all of the requested files could be found in the specified directory")
+  }
+  
+  nFiles<-length(files)
+  out_files<-rep(NA,nFiles)
+  
+  for(i in 1:nFiles){  
+    
+    system(paste(exe,addCommas(files[i]), sep = " "), show.output.on.console = FALSE)
+    
+    #Grab the name of the ouput file from the simulation file
+    out_files[i]<-paste(xmlAttrs(xmlParse(files[i])[["//simulation"]])[[1]],".out",sep="")
+  }
+  
+
+  results<-vector("list",nFiles)
+  skipline<-1
+  for(i in 1:nFiles){
+    res<-try(read.table(out_files[i],skip=skipline,header=T),TRUE)
+    
+    while(class(res)=="try-error" & skipline < 50){
+      skipline<-skipline+1
+      res<-try(read.table(out_files[i],skip=skipline,header=T),TRUE)
+    }
+    
+    res<-res[-1,]
+    
+    if("Date"%in%colnames(res)) res$Date<-dmy(res$Date)
+    
+    for(j in 2:ncol(res)){
+      res[,j]<-as.numeric(as.character(res[,j])) #Coerce each output to be numeric
+    }
+    class(res)<-c("apsim","data.frame")
+    results[[i]]<-res
+  }
+  
+  setwd(oldWD)
+  
+  if(nFiles==1){return(res)
+  }else{
+    names(results)<-gsub(".apsim$","",files)
+    return(results)
+  }
+  
+  
+}
+
+#' Access example APSIM simulations
+#' 
+#' There are quite a few standard APSIM simulations provided in the default APSIM installation.
+#' \code{apsim_expample} moves those example files into the working directory \code{wd} so you can run them
+#' or edit them using \code{\link{apsim}} and \code{\link{edit_apsim}}, respectively.  Generally the
+#' example simulations must be moved because the output file is written to the directory containing
+#' the .apsim file and the ability to write in the "Program Files" folder is limited in most cases.
+#' 
+#' 
+#' @name example_apsim
+#' @param path path to the APSIM installation
+#' @param wd working directory containing the .apsim files to be copied; defaults to the current working directory
+#' @param files files to extract from the "Examples" folder
+#' @param ... additional arguments passed to \code{\link[base:file.copy]{file.copy}}
+#' @return logical; if \code{TRUE} the corresponding file was successfully copied, \code{FALSE} otherwise
+#' @export
+#' @examples
+#' 
+#' \dontrun{
+#' path <-"C:/Program Files (x86)/Apsim76-r3376/"
+#' wd <- "~/APSIM"
+#' file <- "Canopy.apsim"
+#' example_apsim(path = path, wd = wd, file) #TRUE
+#' 
+#' file <- c("Canopy.apsim", "Continuous Wheat.apsim")
+#' example_apsim(path = path, wd = wd, file) #TRUE TRUE
+#' 
+#' exe <-"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe"
+#' results <- apsim(exe, wd, files = file)
+#' }
+
+example_apsim<-function(path, wd = getwd(), files = NULL,...){
+  
+  oldWD<-getwd()
+  setwd(paste(path,"Examples/",sep="/"))
+  
+  possibles <- dir(,pattern=".apsim$")
+  
+  
+  if(length(possibles)==0){
+    stop("There are no .apsim files in the folder wd to copy.")
+  }
+  
+  if(is.null(files)){
+    
+    sim_file_name<-possibles
+    
+  }else{
+    
+    if(!all(files %in% possibles)){
+      stop("One or more of the requested simulations are not in the specified folder.")
+    }
+    sim_file_name<-files
+    
+  }
+  
+  res<-rep(NA,length(files))
+  for(i in 1:length(files)){
+    fromI<-paste(path,"Examples",sim_file_name[i],sep="/")
+    toI<-paste(wd,sim_file_name[i],sep="/")
+    res[i]<-file.copy(from=fromI,to=toI,overwrite=TRUE)
+  }
+  setwd(oldWD) #Return to origninal working directory
+  return(res)
+}
